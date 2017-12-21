c9f59fd1f7135e05d0f76fb0a32e2e2c48714f4f
qiu997018209 <997018209@qq.com>
2017-12-21 13:42:56 +0800

    RNN+Attention

---
+++ 05.三星机器人对话考题优化版本/版本5_双向RNN+Attention_准确率45%/MyWork4/conf.py
@@ -0,0 +1,45 @@
+#coding:utf-8
+'''
+Created on 2017年12月15日
+
+@author: qiujiahao
+
+@email:997018209@qq.com
+
+'''
+#通用模型参数
+#词汇表大小
+vocab_size=5000
+
+num_class=1000
+
+batch_size=128
+#clip gradients at this value
+grad_clip=5.0
+
+num_epochs=100
+#注意力模型
+attn_size=200
+#句子最大长度
+max_document_lenth=0
+# 每多少轮次将训练结果写入tensorboard scalar
+save_per_batch=100
+print_per_batch=100
+
+learning_rate=1e-3
+dropout_keep_prob=0.5
+l2_reg_lambda=0
+embedding_size=128
+
+#CNN模型
+filter_sizes="1,2,3,4,5"
+num_filters=128
+#RNN模型参数
+#隐藏层层数
+num_layers=2
+#隐藏层神经元,或者说是RNN的大小，它应该与embedding_size大小一致
+hidden_dim=128
+# lstm 或 gru
+rnn = 'lstm' 
+
+        
---
+++ 05.三星机器人对话考题优化版本/版本5_双向RNN+Attention_准确率45%/readme.txt
@@ -0,0 +1,4 @@
+双向RNN+Attention
+
+亮点:加入Attention之后准确率有3个点的提升，但是仍然没有CNN效果好。与CNN做模型融合效果比RNN做模型融合要好，
+	但是仍然低于单独做CNN
